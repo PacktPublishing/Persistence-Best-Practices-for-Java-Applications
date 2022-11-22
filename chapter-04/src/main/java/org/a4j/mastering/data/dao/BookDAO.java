ac4a9c5fd8e5893705cb7e69fc7732bc871a46e9
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-22 16:35:41 +0000

    create book supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter-04/src/main/java/org/a4j/mastering/data/dao/BookDAO.java
@@ -0,0 +1,16 @@
+package org.a4j.mastering.data.dao;
+
+import org.a4j.mastering.data.Book;
+
+import java.util.Optional;
+
+public interface BookDAO {
+
+    Optional<Book> findByTitle(String title);
+
+    void insert(Book book);
+
+    void update(Book book);
+
+    void deleteByTitle(String title);
+}
