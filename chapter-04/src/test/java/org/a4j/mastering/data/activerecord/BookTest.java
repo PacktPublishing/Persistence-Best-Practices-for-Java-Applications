25fe6664ca946448d71ec2e33b2a2499caafa0b0
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-23 06:32:13 +0000

    Update book test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-04/src/test/java/org/a4j/mastering/data/activerecord/BookTest.java
+++ chapter-04/src/test/java/org/a4j/mastering/data/activerecord/BookTest.java
@@ -1,9 +1,27 @@
 package org.a4j.mastering.data.activerecord;
 
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookTest {
 
 
+    @Test
+    public void shouldInsert() {
+        Book book = BookSupplier.INSTANCE.get();
+        book.insist();
+        Book model = Book.findById(book.getId());
+        Assertions.assertNotNull(model);
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(book.getId()).isEqualTo(model.getId());
+            softly.assertThat(book.getAuthor()).isEqualTo(model.getAuthor());
+            softly.assertThat(book.getGenre()).isEqualTo(model.getGenre());
+            softly.assertThat(book.getPublisher()).isEqualTo(model.getPublisher());
+        });
+    }
 
 }
