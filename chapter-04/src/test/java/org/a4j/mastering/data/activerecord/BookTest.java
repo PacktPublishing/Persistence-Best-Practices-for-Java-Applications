216b6cf4b94cc47a882696f4fc48888925f51729
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-23 06:33:10 +0000

    fix import at active record
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-04/src/test/java/org/a4j/mastering/data/activerecord/BookTest.java
+++ chapter-04/src/test/java/org/a4j/mastering/data/activerecord/BookTest.java
@@ -4,8 +4,6 @@ import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BookTest {
 
 
