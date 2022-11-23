ee50370c65a5a0bc86be00790bce54bab3edeed6
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-23 10:16:52 +0000

    update book memmory
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-04/src/main/java/org/a4j/mastering/data/dao/BookMemory.java
+++ chapter-04/src/main/java/org/a4j/mastering/data/dao/BookMemory.java
@@ -1,6 +1,7 @@
 package org.a4j.mastering.data.dao;
 
 import org.a4j.mastering.data.Book;
+import org.a4j.mastering.data.mapper.BookMapper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,36 +10,29 @@ import java.util.Optional;
 
 public class BookMemory implements BookDAO {
 
-    private final Map<String, Book> data;
+    private final BookMapper mapper;
 
     public BookMemory() {
-        this.data = new HashMap<>();
+        this.mapper = new BookMapper();
     }
 
     @Override
     public Optional<Book> findByTitle(String title) {
-        Objects.requireNonNull(title, "title is required");
-        return Optional.ofNullable(this.data.get(title));
+        return mapper.findById(title);
     }
 
     @Override
     public void insert(Book book) {
-        Objects.requireNonNull(book, "book is required");
-        this.data.put(book.getTitle(), book);
+        this.mapper.insert(book);
     }
 
     @Override
     public void update(Book book) {
-        Objects.requireNonNull(book, "book is required");
-        if (this.data.get(book.getTitle()) == null) {
-            throw new IllegalArgumentException("You cannot update the information that is not in the database");
-        }
-        this.data.put(book.getTitle(), book);
+        mapper.update(book);
     }
 
     @Override
     public void deleteByTitle(String title) {
-        Objects.requireNonNull(title, "title is required");
-        this.data.remove(title);
+        this.mapper.delete(title);
     }
 }
