04462b0feca229f404bb24de3f23287529000a88
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-23 06:22:10 +0000

    update supplier on the active record
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter-04/src/test/java/org/a4j/mastering/data/activerecord/BookSupplier.java
@@ -0,0 +1,23 @@
+package org.a4j.mastering.data.activerecord;
+
+import com.github.javafaker.Faker;
+
+import java.util.function.Supplier;
+
+enum BookSupplier implements Supplier<Book> {
+    INSTANCE;
+
+    private final Faker faker = new Faker();
+
+    @Override
+    public Book get() {
+        com.github.javafaker.Book book = faker.book();
+
+        return Book.builder()
+                .title(book.title())
+                .author(book.author())
+                .genre(book.genre())
+                .publisher(book.publisher())
+                .build();
+    }
+}
---
+++ chapter-04/src/test/java/org/a4j/mastering/data/activerecord/BookTest.java
@@ -0,0 +1,9 @@
+package org.a4j.mastering.data.activerecord;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+
+
+}
