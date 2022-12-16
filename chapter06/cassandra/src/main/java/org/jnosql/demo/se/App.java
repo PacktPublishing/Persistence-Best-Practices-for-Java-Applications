1be06f2f73a22715ca4dd09361319cc0d7b37bf7
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-16 19:00:52 -0300

    create modules for chapter 06
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter06/cassandra/src/main/java/org/jnosql/demo/se/App.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package org.jnosql.demo.se;
+
+
+import org.eclipse.jnosql.mapping.cassandra.column.CassandraTemplate;
+import jakarta.nosql.mapping.column.ColumnTemplate;
+import jakarta.nosql.column.ColumnQuery;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static jakarta.nosql.column.ColumnQuery.select;
+
+
+public class App {
+
+    private static final Person PERSON = Person.builder().
+            phones(Arrays.asList("234", "432"))
+            .name("Name")
+            .id(1)
+            .ignore("Just Ignore")
+            .build();
+
+    public static void main(String[] args) {
+
+        try(SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+            ColumnTemplate template =  container.select(CassandraTemplate.class).get();
+            Person saved = template.insert(PERSON);
+            System.out.println("Person saved" + saved);
+
+            ColumnQuery query = select().from("Person").where("id").eq(1L).build();
+
+            Optional<Person> person = template.singleResult(query);
+            System.out.println("Entity found: " + person);
+
+        }
+    }
+
+    private App() {}
+}
---
+++ chapter06/cassandra/src/main/java/org/jnosql/demo/se/App5.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+package org.jnosql.demo.se;
+
+
+import org.eclipse.jnosql.mapping.cassandra.column.CassandraTemplate;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Optional;
+
+public class App5 {
+
+
+    public static void main(String[] args) {
+
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+
+            CassandraTemplate template = container.select(CassandraTemplate.class).get();
+            Currency currency = Currency.getInstance(Locale.US);
+
+            Company company = Company.builder()
+                    .name("SouJava")
+                    .add(Headquarter.of("Salvador", "Brazil"))
+                    .add(Headquarter.of("Sao Paulo", "Brazil"))
+                    .add(Headquarter.of("Leiria", "Portugal"))
+                    .build();
+
+            template.insert(company);
+            Optional<Company> soujava = template.find(Company.class, "SouJava");
+
+            System.out.println("the company is " + soujava);
+
+
+        }
+    }
+
+    private App5() {
+    }
+}
