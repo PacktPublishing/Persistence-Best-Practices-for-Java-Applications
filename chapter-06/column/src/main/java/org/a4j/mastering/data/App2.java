13669ce1cfea5e6c06685102fbcdc88584e53799
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-16 20:31:53 -0300

    Update package name
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter-06/column/src/main/java/org/a4j/mastering/data/App2.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package org.a4j.mastering.data;
+
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.eclipse.jnosql.mapping.DatabaseQualifier.ofColumn;
+
+public class App2 {
+
+    private static final Person PERSON = Person.builder()
+            .id(1)
+            .phones(Arrays.asList("234", "432"))
+            .name("Name")
+            .ignore("Just Ignore")
+            .build();
+    public static void main(String[] args) {
+
+        try(SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+            PersonRepository repository = container.select(PersonRepository.class).select(ofColumn()).get();
+            Person saved = repository.save(PERSON);
+            System.out.println("Person saved" + saved);
+
+            Optional<Person> person = repository.findById(1L);
+            System.out.println("Entity found: " + person);
+
+        }
+    }
+
+
+    private App2() {}
+}
---
+++ chapter-06/column/src/main/java/org/a4j/mastering/data/PersonBuilder.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+package org.a4j.mastering.data;
+
+import java.util.List;
+
+public class PersonBuilder {
+    private long id;
+    private String name;
+    private List<String> phones;
+    private String ignore;
+
+    PersonBuilder() {
+    }
+
+    public PersonBuilder id(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public PersonBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public PersonBuilder phones(List<String> phones) {
+        this.phones = phones;
+        return this;
+    }
+
+    public PersonBuilder ignore(String ignore) {
+        this.ignore = ignore;
+        return this;
+    }
+
+    public Person build() {
+        return new Person(id, name, phones, ignore);
+    }
+}
