47b1587ee2b5031bfc2d5c84f1969a6ff1ccfeac
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-17 17:21:29 -0300

    define book constructor
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-06/column/src/main/java/org/a4j/mastering/data/Book.java
+++ chapter-06/column/src/main/java/org/a4j/mastering/data/Book.java
@@ -4,6 +4,7 @@ package org.a4j.mastering.data;
 import jakarta.nosql.mapping.Column;
 import jakarta.nosql.mapping.Entity;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -16,4 +17,45 @@ public class Book {
     @Column
     private String title;
 
+    Book() {
+    }
+
+    public Book(UUID id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
+    }
 }
