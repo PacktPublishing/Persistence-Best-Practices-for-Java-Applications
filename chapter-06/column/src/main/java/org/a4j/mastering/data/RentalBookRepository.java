4a728be001105e62f3cf47e612c12abba743d34a
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-17 19:31:57 -0300

    update repository to use cassandra repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-06/column/src/main/java/org/a4j/mastering/data/RentalBookRepository.java
+++ chapter-06/column/src/main/java/org/a4j/mastering/data/RentalBookRepository.java
@@ -1,10 +1,10 @@
 package org.a4j.mastering.data;
 
-import jakarta.nosql.mapping.Repository;
+import org.eclipse.jnosql.mapping.cassandra.column.CassandraRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.util.UUID;
 
 @ApplicationScoped
-public interface RentalBookRepository extends Repository<RentalBook, UUID> {
+public interface RentalBookRepository extends CassandraRepository<RentalBook, UUID> {
 }
