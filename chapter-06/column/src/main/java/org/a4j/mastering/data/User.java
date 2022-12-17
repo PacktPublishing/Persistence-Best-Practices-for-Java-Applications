ff51a173980aab7c1ffc5487630146fe49ff87da
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-17 17:04:21 -0300

    create user udt
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter-06/column/src/main/java/org/a4j/mastering/data/User.java
@@ -0,0 +1,11 @@
+package org.a4j.mastering.data;
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+
+@Entity
+public class User {
+
+    @Column
+    private String username;
+}
