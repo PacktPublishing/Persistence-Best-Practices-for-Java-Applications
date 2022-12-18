40c8e7815ef53a084230439c333c4db9365b0925
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-18 06:54:14 -0300

    define author as record
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-06/document/src/main/java/org/a4j/mastering/data/Author.java
+++ chapter-06/document/src/main/java/org/a4j/mastering/data/Author.java
@@ -3,18 +3,7 @@ package org.a4j.mastering.data;
 import jakarta.nosql.mapping.Column;
 import jakarta.nosql.mapping.Entity;
 
-import java.util.Map;
-
 
 @Entity
-public class Author {
-
-    @Column
-    private String nickname;
-
-    @Column
-    private String name;
-
-    @Column
-    private String profile;
+public record Author(@Column("nickname") String nickname, @Column("name") String name, @Column("profile") String profile) {
 }
