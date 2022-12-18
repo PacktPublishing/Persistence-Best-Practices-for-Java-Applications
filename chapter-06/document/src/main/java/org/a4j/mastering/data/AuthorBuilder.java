f0e3650697bb8729c766aa6ddd90200e29dc2dfc
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-18 06:53:34 -0300

    create author builder
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter-06/document/src/main/java/org/a4j/mastering/data/AuthorBuilder.java
@@ -0,0 +1,29 @@
+package org.a4j.mastering.data;
+
+import jakarta.nosql.mapping.Column;
+
+public class AuthorBuilder {
+
+    private String nickname;
+    private String name;
+    private String profile;
+
+    public AuthorBuilder nickname(String nickname) {
+        this.nickname = nickname;
+        return this;
+    }
+
+    public AuthorBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public AuthorBuilder profile(String profile) {
+        this.profile = profile;
+        return this;
+    }
+
+    public Author builder() {
+        return new Author(nickname, name, profile);
+    }
+}
