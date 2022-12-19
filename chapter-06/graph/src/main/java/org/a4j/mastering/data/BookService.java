90e31f04e1956f664e94c2d772ba2edcbf51c5ad
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-19 16:45:52 -0300

    define book service
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter-06/graph/src/main/java/org/a4j/mastering/data/BookService.java
@@ -0,0 +1,24 @@
+package org.a4j.mastering.data;
+
+
+import org.eclipse.jnosql.mapping.graph.GraphTemplate;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.Objects;
+
+@ApplicationScoped
+class BookService {
+
+    @Inject
+    private GraphTemplate template;
+
+
+    public Book save(Book book) {
+        Objects.requireNonNull(book, "book is required");
+        return template.getTraversalVertex().hasLabel(Book.class)
+                .has("name", book.getName())
+                .<Book>next()
+                .orElseGet(() -> template.insert(book));
+    }
+}
