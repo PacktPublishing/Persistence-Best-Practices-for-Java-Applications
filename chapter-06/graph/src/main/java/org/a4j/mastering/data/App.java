db38e9f51ea7b264e307dcd48d8a715685503269
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-19 18:36:02 -0300

    improve graph sample to use template
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-06/graph/src/main/java/org/a4j/mastering/data/App.java
+++ chapter-06/graph/src/main/java/org/a4j/mastering/data/App.java
@@ -10,6 +10,8 @@
  */
 package org.a4j.mastering.data;
 
+import org.eclipse.jnosql.mapping.graph.GraphTemplate;
+
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
 import java.util.List;
@@ -26,41 +28,37 @@ public final class App {
         try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
             LibraryFacade facade = container.select(LibraryFacade.class).get();
 
+            GraphTemplate template = container.select(GraphTemplate.class).get();
+
             Category software = facade.save(Category.of("Software"));
-            Category romance = facade.save(Category.of("Romance"));
 
             Category java = facade.save(Category.of("Java"));
             Category nosql =facade.save(Category.of("NoSQL"));
-            Category microService =  facade.save(Category.of("Micro Service"));
 
             Book effectiveJava = facade.save(Book.of("Effective Java"));
             Book nosqlDistilled =  facade.save(Book.of("NoSQL Distilled"));
-            Book migratingMicroservice =  facade.save(Book.of("Migrating to Microservice Databases"));
-            Book shack = facade.save(Book.of("The Shack"));
 
 
             template.edge(java, "is", software);
             template.edge(nosql, "is", software);
-            template.edge(microService, "is", software);
 
             template.edge(effectiveJava, "is", software);
             template.edge(nosqlDistilled, "is", software);
-            template.edge(migratingMicroservice, "is", software);
 
             template.edge(effectiveJava, "is", java);
             template.edge(nosqlDistilled, "is", nosql);
-            template.edge(migratingMicroservice, "is", microService);
 
 
-            template.edge(shack, "is", romance);
 
-            List<String> softwareCategories = template.getTraversalVertex().hasLabel("Category")
+            List<String> softwareCategories = template.getTraversalVertex()
+                    .hasLabel("Category")
                     .has("name", "Software")
                     .in("is").hasLabel("Category").<Category>getResult()
                     .map(Category::getName)
                     .collect(toList());
 
-            List<String> softwareBooks = template.getTraversalVertex().hasLabel("Category")
+            List<String> softwareBooks = template.getTraversalVertex()
+                    .hasLabel("Category")
                     .has("name", "Software")
                     .in("is").hasLabel("Book").<Book>getResult()
                     .map(Book::getName)
