d10987e044cb1866e3448f90b365d938a22504ab
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-20 17:41:27 -0300

    define method to create edge
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-06/graph/src/main/java/dev/a4j/mastering/data/LibraryFacade.java
+++ chapter-06/graph/src/main/java/dev/a4j/mastering/data/LibraryFacade.java
@@ -1,8 +1,12 @@
 package dev.a4j.mastering.data;
 
 
+import org.eclipse.jnosql.mapping.graph.GraphTemplate;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+
+import static dev.a4j.mastering.data.EdgeLabels.IS;
 
 @ApplicationScoped
 class LibraryFacade {
@@ -10,9 +14,11 @@ class LibraryFacade {
     @Inject
     private BookDAO bookDAO;
 
-
     @Inject
     private CategoryDAO categoryDAO;
+
+    @Inject
+    private GraphTemplate template;
 
     public Book save(Book book) {
         return this.bookDAO.save(book);
@@ -20,5 +26,10 @@ class LibraryFacade {
 
     public Category save(Category category) {
         return this.categoryDAO.save(category);
+    }
+
+
+    public void is(Category category, Book book){
+        template.edge(category, IS, book);
     }
 }
