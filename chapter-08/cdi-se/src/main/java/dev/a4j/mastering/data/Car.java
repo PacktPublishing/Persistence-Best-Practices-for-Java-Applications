05f098680d14c5582ab02bbe74f10e3d69b81da2
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-23 21:32:55 -0300

    convert record car to a class
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-08/cdi-se/src/main/java/dev/a4j/mastering/data/Car.java
+++ chapter-08/cdi-se/src/main/java/dev/a4j/mastering/data/Car.java
@@ -1,9 +1,65 @@
 package dev.a4j.mastering.data;
 
 import java.time.Year;
+import java.util.Objects;
 
-public record Car(String plate, Year year, String name, String model) {
+public class Car{
 
+    private final String make;
+    private final Year year;
+
+    private final String name;
+
+    private final String model;
+
+    Car(String make, Year year, String name, String model) {
+        this.make = make;
+        this.year = year;
+        this.name = name;
+        this.model = model;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return Objects.equals(make, car.make)
+                && Objects.equals(year, car.year)
+                && Objects.equals(name, car.name)
+                && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(make, year, name, model);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "make='" + make + '\'' +
+                ", year=" + year +
+                ", name='" + name + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
 
     public static CarBuilder builder() {
         return new CarBuilder();
