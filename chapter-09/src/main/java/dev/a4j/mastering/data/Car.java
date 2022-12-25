f7db302aeae70dd97257d8e40f3c37285b970305
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-25 11:13:16 -0300

    Add garage domain
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-09/src/main/java/dev/a4j/mastering/data/Car.java
+++ chapter-09/src/main/java/dev/a4j/mastering/data/Car.java
@@ -1,2 +1,18 @@
-package dev.a4j.mastering.data;public class Car {
+package dev.a4j.mastering.data;
+
+
+import jakarta.data.Entity;
+import jakarta.data.Id;
+
+import java.time.Year;
+
+@Entity
+public class Car {
+
+    @Id
+    private String plate;
+
+    private String model;
+
+    private Year year;
 }
--- chapter-09/src/main/java/dev/a4j/mastering/data/Garage.java
+++ chapter-09/src/main/java/dev/a4j/mastering/data/Garage.java
@@ -1,2 +1,9 @@
-package dev.a4j.mastering.data;public interface Garage {
+package dev.a4j.mastering.data;
+
+import jakarta.data.repository.PageableRepository;
+import jakarta.data.repository.Repository;
+
+
+@Repository
+public interface Garage extends PageableRepository<Car, String> {
 }
