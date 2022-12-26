ec848b6a15652a0cdd805f23b1ca08a6bacad2fe
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-26 19:19:32 -0300

    add sample using app
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-09/src/main/java/dev/a4j/mastering/data/App.java
+++ chapter-09/src/main/java/dev/a4j/mastering/data/App.java
@@ -1,2 +1,14 @@
-package dev.a4j.mastering.data;public class App {
+package dev.a4j.mastering.data;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+
+public class App {
+
+    public static void main(String[] args) {
+        try(SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+            Garage garage = container.select(Garage.class).get();
+
+        }
+    }
 }
