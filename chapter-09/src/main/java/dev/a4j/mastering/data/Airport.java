d064add346af53ecc90056f1dcc3b40bfce52fa5
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-27 06:11:39 -0300

    create pagable airport
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-09/src/main/java/dev/a4j/mastering/data/Airport.java
+++ chapter-09/src/main/java/dev/a4j/mastering/data/Airport.java
@@ -1,8 +1,12 @@
 package dev.a4j.mastering.data;
 
+import jakarta.data.repository.Page;
+import jakarta.data.repository.Pageable;
 import jakarta.data.repository.PageableRepository;
 import jakarta.data.repository.Repository;
 
 @Repository
 public interface Airport extends PageableRepository<Airport, String> {
+
+    Page<Airport> findByManufacturer(String manufacturer, Pageable pageable);
 }
--- chapter-09/src/main/java/dev/a4j/mastering/data/App2.java
+++ chapter-09/src/main/java/dev/a4j/mastering/data/App2.java
@@ -27,6 +27,8 @@ public class App2 {
                     .build();
 
 
+
+
         }
     }
 }
