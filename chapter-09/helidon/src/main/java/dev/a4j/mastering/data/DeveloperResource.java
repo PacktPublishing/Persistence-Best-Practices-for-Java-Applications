15e71c075690d6c14968a312796399041acd7083
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-28 06:46:08 -0300

    update developer resource
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-09/helidon/src/main/java/dev/a4j/mastering/data/DeveloperResource.java
+++ chapter-09/helidon/src/main/java/dev/a4j/mastering/data/DeveloperResource.java
@@ -4,10 +4,16 @@ package dev.a4j.mastering.data;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,4 +36,23 @@ public class DeveloperResource {
     public List<Developer> getDevelopers() {
         return this.repository.findAll().collect(Collectors.toUnmodifiableList());
     }
+
+    @GET
+    @Path("{id}")
+    public Developer findById(@PathParam("id") String id) {
+        return this.repository.findById(id)
+                .orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
+    }
+
+    @PUT
+    public Developer insert(Developer developer) {
+        return this.repository.save(developer);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteById(@PathParam("id") String id) {
+        this.repository.deleteById(id);
+    }
+
 }
