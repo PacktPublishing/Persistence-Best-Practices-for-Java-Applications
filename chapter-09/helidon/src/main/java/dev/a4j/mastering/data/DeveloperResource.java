17e59fc63f7082d2dd28ba0bc99c22e582f5ea41
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-28 06:46:45 -0300

    remove import at developer resource
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-09/helidon/src/main/java/dev/a4j/mastering/data/DeveloperResource.java
+++ chapter-09/helidon/src/main/java/dev/a4j/mastering/data/DeveloperResource.java
@@ -6,7 +6,6 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
