171f716d79501b31f4384bf11d9ba773cf67ae20
Otavio Santana <otaviopolianasantana@gmail.com>
2022-12-28 09:31:13 -0300

    update repository project
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-04/src/main/java/dev/a4j/mastering/data/RepositoryApp.java
+++ chapter-04/src/main/java/dev/a4j/mastering/data/RepositoryApp.java
@@ -5,14 +5,15 @@ import dev.a4j.mastering.data.db.BookSupplier;
 import dev.a4j.mastering.data.repository.Library;
 import dev.a4j.mastering.data.repository.LibraryMemory;
 
+import java.util.Optional;
+
 public class RepositoryApp {
 
     public static void main(String[] args) {
         Book book = BookSupplier.INSTANCE.get();
         Library library = new LibraryMemory(new BookMemory());
         library.save(book);
-        Book entity = library.findByTitle(book.getTitle())
-                .orElseThrow();
+        Optional<Book> entity = library.findByTitle(book.getTitle());
         System.out.println("the entity result: " + entity);
     }
 }
